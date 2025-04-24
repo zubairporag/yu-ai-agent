0a24cff51be771c476b883d5f508886a2b3f0696
1-鱼皮 <592789970@qq.com>
2025-04-24 17:42:21 +0800

    第 2 期 - AI 大模型接入 1. 新建项目，完成后端初始化 2. 整合 Hutool 和 Knife4j 依赖 3. 整合 Spring AI、LangChain4j 等框架，实现 4 种程序调用 AI 大模型的示例代码

---
+++ src/main/java/com/yupi/yuaiagent/YuAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.yuaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiAgentApplication.class, args);
+    }
+
+}
